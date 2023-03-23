5ff805b299b3145e457f94de1f9b835999c164ae
Nnanke Otu <otunnanke@gmail.com>
2023-03-23 05:14:07 -0700

    more numbers again

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints set of numbers 10x
+ *
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

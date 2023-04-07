fdd3e92e15ad01e7f720ce5137f5f7284bfb0848
Nnanke Otu <otunnanke@gmail.com>
2023-04-07 16:47:15 -0700

    argsc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argc: command line count
+ * @argv: command variable count
+ *
+ * Return: 0(successful)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+	printf("argc = %d\n", argc);
+	return (0);
+}

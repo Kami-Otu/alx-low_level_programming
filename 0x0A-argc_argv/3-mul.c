a9c754bc4d7db1d22a8a8d2c40f43b6ae8788b1e
Nnanke Otu <otunnanke@gmail.com>
2023-04-07 19:02:54 -0700

    muls

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,8 +14,11 @@ int main(int argc, char *argv[])
 {
 	int i, result = 1;
 
-	if (argc < 1)
-		printf("Error");
+	if (argc > 3 || argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	else if (argc >= 1)
 	{
 		for (i = 1; i < argc; i++)
